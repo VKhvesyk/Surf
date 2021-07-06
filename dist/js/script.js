4cf94d25161754597c155cd5ee12855c88e095b0
VKhvesyk <khvesyk.vasyl@gmail.com>
2021-07-06 20:51:40 +0300

    Added shop section, video jquery plagin for video in experience section. Deleted not used js, css files

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,17 +1,52 @@
 
-$(document).ready(function(){
-  $('.slider').slick({
-    adaptiveHeight: true,
-    slidesToShow: 1,
-    arrows: true,
-    nextArrow: document.querySelector('.main-sliderControl__right-arrow'),
-    prevArrow: document.querySelector('.main-sliderControl__left-arrow'),
-    dotsClass: document.querySelector('.main-sliderControl__dots')
+
+window.onload = function() {
+  
+  $(document).ready(function(){
+    $('.slider').slick({
+      adaptiveHeight: true,
+      slidesToShow: 1,
+      arrows: true,
+      nextArrow: document.querySelector('.main-sliderControl__right-arrow'),
+      prevArrow: document.querySelector('.main-sliderControl__left-arrow'),
+      dotsClass: document.querySelector('.main-sliderControl__dots')
+    });
   });
-});
+  
+  
+  $(".slider").on("afterChange", function(event, slick, currentSlide, nextSlide, prevSlide){
+    $(".dynamic-slide-number").text(currentSlide + 1);
+  });
+  
+  let clientHeight = document.getElementById('body').clientHeight;
+  console.log(clientHeight);
+  
+  document.getElementById('line').style.height = `${clientHeight}px`;
 
 
-$(".slider").on("afterChange", function(event, slick, currentSlide, nextSlide, prevSlide){
-  $(".dynamic-slide-number").text(currentSlide + 1);
-});
+
+  $("document").ready(function() {
+    $("#video").simplePlayer();
+  });
+
+  // $("#video").simplePlayer({
+
+  //   autoplay: 1,
+  //   autohide: 1,
+  //   border: 0,
+  //   wmode: 'opaque',
+  //   enablejsapi: 1,
+  //   modestbranding: 1,
+  //   version: 3,
+  //   hl: 'en_US',
+  //   rel: 0,
+  //   showinfo: 0,
+  //   hd: 1,
+  //   iv_load_policy: 3 // add origin
+    
+  //   });
+
+
+
+};
 
