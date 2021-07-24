9bff7709ba415abb9e6279f1f29074824cddfc48
VKhvesyk <khvesyk.vasyl@gmail.com>
2021-07-24 19:38:33 +0300

    Slick slider has been replaced by my own script. Changed cards in the shop section. Added ability to show/hide cards

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,34 +1,127 @@
 
 
 window.onload = function() {
-  
-  $(document).ready(function(){
-    $('.slider').slick({
-      adaptiveHeight: true,
-      slidesToShow: 1,
-      arrows: true,
-      nextArrow: document.querySelector('.main-sliderControl__right-arrow'),
-      prevArrow: document.querySelector('.main-sliderControl__left-arrow'),
-      dotsClass: document.querySelector('.main-sliderControl__dots')
-    });
-  });
-  
-  
-  $(".slider").on("afterChange", function(event, slick, currentSlide, nextSlide, prevSlide){
-    $(".dynamic-slide-number").text(currentSlide + 1);
-  });
-  
- 
+
 
   //Автоматичне встановлення висоти горизонтальних ліній
 
-  let siteHeight = document.querySelector('body').clientHeight,
+    let siteHeight = document.querySelector('body').clientHeight,
         line = document.querySelector('.lines');
-  console.log(siteHeight);
+
+    console.log(siteHeight); // only for test
+
+    line.style.height = `${siteHeight}px`;
   
-  line.style.height = `${siteHeight}px`;
+  // Slider
+
+    const slides = document.querySelectorAll('.main-slide'),
+          prev = document.querySelector('.main-sliderControl__left-arrow'),
+          next = document.querySelector('.main-sliderControl__right-arrow'),
+          total = document.querySelector('#total'),
+          current = document.querySelector('#current'),
+          slidesWrapper = document.querySelector('.main-viewport'),
+          slidesField = document.querySelector('.main-viewport__inner'),
+          width = window.getComputedStyle(slidesWrapper).width;
 
 
+    let slideIndex = 1;
+    let offset = 0;
+
+    total.textContent = slides.length;
+    current.textContent = slideIndex;
+
+
+    slidesField.style.width = 100 * slides.length + '%';
+    slidesField.style.display = 'flex';
+    slidesField.style.transition = '0.5s all';
+
+    slidesWrapper.style.overflow = 'hidden';
+
+
+    slides.forEach(slide => {
+        slide.style.width = width;
+    });
+
+
+    next.addEventListener('click', () => {
+        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+            offset = 0;
+        } else {
+            offset += +width.slice(0, width.length - 2);
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == slides.length) {
+            slideIndex = 1;
+        } else {
+            slideIndex++;
+        }
+
+        current.textContent = slideIndex;
+    });
+
+    prev.addEventListener('click', () => {
+        if (offset == 0) {
+            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+        } else {
+            offset -= +width.slice(0, width.length - 2);
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == 1) {
+            slideIndex = slides.length;
+        } else {
+            slideIndex--;
+        }
+
+        current.textContent = slideIndex;
+    });
+
+
+  // Show hide shop cards  
+ 
+  const showButton = document.querySelector('.shop__show-cards'),
+        cards = document.querySelectorAll('.shop-cards');
+  let clickNumber = 2;
+
+  showButton.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      // cards.forEach(card => {
+      //   card.classList.remove('hide');
+      // });
+      console.log(event);
+
+      // for (let i = 3; i < cards.length; i++) {
+      //   cards[i].classList.toggle('hide');
+      //   cards[i].classList.toggle('animation'); 
+      // }
+
+      
+        if (clickNumber % 2 == 0) {
+          for (let i = 3; i < cards.length; i++) {
+            cards[i].classList.toggle('hide');
+            cards[i].classList.toggle('animation'); 
+          }
+          showButton.textContent = 'Show less';
+          clickNumber++;
+        } else {
+          for (let i = 3; i < cards.length; i++) {
+            cards[i].classList.toggle('hide');
+            cards[i].classList.toggle('animation'); 
+          }
+          showButton.textContent = 'Show all';
+          clickNumber++;
+        }
+      
+  });
+
+
+
+
+  // Player
 
   $("document").ready(function() {
     $("#video").simplePlayer();
